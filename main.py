40a3fce59b214a808db73d6d81087b98e0dbe9b4
kamperemu <pvedang388@gmail.com>
2020-11-16 09:53:14 +0300

    json file is now usable

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import os
-
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
 import tensorflow
+import json
+
+with open('test.json','r') as json_file:
+    data = json.load(json_file)
+    print(len(data))

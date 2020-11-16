1f31ef4e25681196c2b9f9d35315546a4977810f
kamperemu <pvedang388@gmail.com>
2020-11-16 09:54:48 +0300

    moved test.json back to datasets

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 import tensorflow
 import json
 
-with open('test.json','r') as json_file:
+with open('datasets/test.json','r') as json_file:
     data = json.load(json_file)
     print(len(data))

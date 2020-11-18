328c607ea31d359e01f1b4f24deea644bf86b9b5
kamperemu <pvedang388@gmail.com>
2020-11-18 09:00:21 +0300

    data

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 import tensorflow
 import json
-
+data=[]
 with open('datasets/test.json','r') as json_file:
     data = json.load(json_file)
-    print(len(data))
+
+sum0 = 0
+sum1 = 0
+for i in data:
+    if i["label"]==0:
+        sum0+=1
+    if i["label"]==1:
+        sum1+=1
+
+print(sum0)
+print(sum1)

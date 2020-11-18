c1d5fe5bee58de69b64b1cf75e68863f0db295e6
kamperemu <pvedang388@gmail.com>
2020-11-18 12:28:08 +0300

    hello

--- main.py
+++ main.py
@@ -7,14 +7,32 @@ data=[]
 with open('datasets/test.json','r') as json_file:
     data = json.load(json_file)
 
-sum0 = 0
-sum1 = 0
+xtrain = []
+ytrain = []
+xtest = []
+ytest = []
+sum0=0
+sum1=0
 for i in data:
-    if i["label"]==0:
-        sum0+=1
-    if i["label"]==1:
-        sum1+=1
+    if i["label"] == 0:
+        if sum0<=4000:
+            xtrain.append(i["content"])
+            ytrain.append(i["label"])
+        else:
+            xtest.append(i["content"])
+            ytest.append(i["label"])
+        sum0 += 1
+    if i["label"] == 1:
+        if sum1<=4000:
+            xtrain.append(i["content"])
+            ytrain.append(i["label"])
+        else:
+            xtest.append(i["content"])
+            ytest.append(i["label"])
+        sum1 += 1
+        
+for i in range(len(xtrain)):
+    print(ytrain[i], " ", xtrain[i])
 
-print(sum0)
-print(sum1)
-print(sum0+sum1)
+for i in range(len(xtest)):
+    print(ytest[i], " ", xtest[i])

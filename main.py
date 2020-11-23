61da7f07cbcfb280c2a5afef2d80ded5b86b370e
kamperemu <pvedang388@gmail.com>
2020-11-23 13:22:56 +0300

    hwllofefe

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+# Importing the required modules
+
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-
 import tensorflow
 import json
+
+
+# Making an array from the dataset file
+
 data=[]
 with open('datasets/test.json','r') as json_file:
     data = json.load(json_file)
+
+# Classifying the training and testing, input and output of the dataset
 
 xtrain = []
 ytrain = []
@@ -30,12 +37,20 @@ for i in data:
             xtest.append(i["content"])
             ytest.append(i["label"])
         sum1 += 1
-        
+     
+     
+# Converting the input layer to ASCII values.
 
+xtrainord = []
 for j in range(len(xtrain)):
+    xtrainord.append([])
     for i in xtrain[j]:
-        print(ord(i),end=" ")
-print()
+        xtrainord[j].append(ord(i))
+xtestord = []
 for j in range(len(xtest)):
+    xtestord.append([])
     for i in xtest[j]:
-        print(ord(i),end=" ")
+        xtestord[j].append(ord(i))
+
+print(xtrainord)
+print(xtestord)

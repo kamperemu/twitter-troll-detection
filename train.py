a035e2f56f14871e88a477f62e11203a0aef3f4c
kamperemu <pvedang388@gmail.com>
2020-11-25 12:50:06 +0300

    hkfjhakg.sdghkadhflk

--- train.py
+++ train.py
@@ -2,7 +2,23 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-dataset = tf.data.TextLineDataset("datasets/test.csv")
+def filter_train(line):
+    split_line = tf.strings.split(line,",", maxsplit=3)
+    dataset_belonging = split_line[1]
+    
+    return (True if dataset_belonging == 'train' else False)
 
-for line in dataset.skip(1).take(5):
+
+def filter_test(line):
+    split_line = tf.strings.split(line,",", maxsplit=3)
+    dataset_belonging = split_line[1]
+    
+    return (True if dataset_belonging == 'test' else False)
+
+
+train = tf.data.TextLineDataset("datasets/test.csv").filter(filter_train)
+test = tf.data.TextLineDataset("datasets/test.csv").filter(filter_test)
+
+
+for line in test.skip(1).take(5):
     print(tf.strings.split(line, ",", maxsplit=3))

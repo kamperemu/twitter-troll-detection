62c3213cd2ca995ac10503f6238287af26320df4
kamperemu <34961229+kamperemu@users.noreply.github.com>
2020-11-28 16:59:05 +0300

    Add files via upload

--- train.py
+++ train.py
@@ -1,70 +1,81 @@
 # removes default logs
-import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+# import os
+# os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 # imports required for the training algorithm
 import tensorflow as tf
-import pandas as pd
-from tensorflow import keras
-from tensorflow.keras import layers
 from tensorflow.keras.preprocessing.text import Tokenizer
-import pickle
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+import json
+
+# predefined variables
+vocab_size = 10000
+embedding_dim = 16
+max_length = 200
+trunc_type='post'
+padding_type='post'
+oov_tok = "<OOV>"
 
 
-# filtering based on training data and testing data
-def filter_train(line):
-    datatype = tf.strings.split(line,",", maxsplit=3)[1]
-    return (True if datatype == 'train' else False)
+# training data and testing data
+with open("datasets/test.json", 'r') as f:
+    datastore = json.load(f)
+
+xtrain = []
+ytrain = []
+xtest = []
+ytest = []
+
+for item in datastore:
+    if item["type"] == "train":
+        xtrain.append(item['content'])
+        ytrain.append(item['label'])
+    if item["type"] == "test":
+        xtest.append(item['content'])
+        ytest.append(item['label'])
 
 
-def filter_test(line):
-    datatype = tf.strings.split(line,",", maxsplit=3)[1]
-    return (True if datatype == 'test' else False)
+# Tokenization go brrr
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(xtrain)
+
+word_index = tokenizer.word_index
+
+# preparing training data for neural network
+xtrainencoded = tokenizer.texts_to_sequences(xtrain)
+xtrainpadded = pad_sequences(xtrainencoded, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+xtrain = np.asarray(xtrainpadded).astype(np.float32)
+ytrain = np.asarray(ytrain).astype(np.float32)
+
+# preparing testing data for neural network
+xtestencoded = tokenizer.texts_to_sequences(xtest)
+xtestpadded = pad_sequences(xtestencoded, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+xtest = np.asarray(xtestpadded).astype(np.float32)
+ytest = np.asarray(ytest).astype(np.float32)
+
+# neural network
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+num_epochs = 30
+history = model.fit(xtrain, ytrain, epochs=num_epochs, validation_data=(xtest, ytest), verbose=2)
 
 
-train = tf.data.TextLineDataset("datasets/test.csv").filter(filter_train)
-test = tf.data.TextLineDataset("datasets/test.csv").filter(filter_test)
-
-tweets = []
-for line in train.skip(1).take(5):
-    split_line = tf.strings.split(line, ",", maxsplit=3)
-    tweet = split_line[2]
-    tweet = tweet.numpy()
-    tweets.append(tweet)
+import matplotlib.pyplot as plt
 
 
-
-
-
-'''
-tf_keras_tokenizer.fit_on_texts(text_data)
-tf_keras_encoded = tf_keras_tokenizer.texts_to_sequences(text_data)
-tf_keras_encoded = pad_sequences(tf_keras_encoded, padding="post") 
-
-AUTOTUNE = tf.data.experimental.AUTOTUNE
-train = train.map(encode_map_fn, num_parallel_calls=AUTOTUNE).cache()
-train = train.shuffle(25000)
-train = train.padded_batch(32, padded_shapes=([None], ()))
-
-test = test.map(encode_map_fn)
-test = test.padded_batch(32, padded_shapes=([None], ()))
-
-model = keras.Sequential(
-    [
-        layers.Masking(mask_value=0),
-        layers.Embedding(input_dim=len(vocabulary) + 2, output_dim=32,),
-        layers.GlobalAveragePooling1D(),
-        layers.Dense(64, activation="relu"),
-        layers.Dense(1),
-    ]
-)
-
-model.compile(
-    loss=keras.losses.BinaryCrossentropy(from_logits=True),
-    optimizer=keras.optimizers.Adam(3e-4, clipnorm=1),
-    metrics=["accuracy"],
-)
-
-model.fit(train, epochs=15, verbose=2)
-model.evaluate(test)
-'''
+def plot_graphs(history, string):
+  plt.plot(history.history[string])
+  plt.plot(history.history['val_'+string])
+  plt.xlabel("Epochs")
+  plt.ylabel(string)
+  plt.legend([string, 'val_'+string])
+  plt.show()
+  
+plot_graphs(history, "accuracy")
+plot_graphs(history, "loss")

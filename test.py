8ca66bfe31834a70d6531c43ceb523b49a418b38
kamperemu <pvedang388@gmail.com>
2020-12-05 12:16:15 +0300

    training go brr

---
+++ test.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
--- train.py
+++ train.py
@@ -9,13 +9,17 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
 import json
 
+
+
+
+
 # predefined variables
-vocab_size = 10000
-embedding_dim = 16
-max_words = 200
-trunc_type='post'
-padding_type='post'
-oov_tok = "<OOV>"
+vocabSize = 10000
+outputDim = 16
+maxInput = 200
+truncType='post'
+padType='post'
+oov = "<OOV>"
 
 
 # training data and testing data
@@ -36,32 +40,53 @@ for tweet in tweets:
         ytest.append(tweet['label'])
 
 
-# Tokenization go brrr
-tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+# tokenization go brrr
+tokenizer = Tokenizer(num_words=vocabSize, oov_token=oov)
 tokenizer.fit_on_texts(xtrain)
 
-word_index = tokenizer.word_index
+wordIndex = tokenizer.word_index
 
 # preparing training data for neural network
 xtrainencoded = tokenizer.texts_to_sequences(xtrain)
-xtrainpadded = pad_sequences(xtrainencoded, maxlen=max_words, padding=padding_type, truncating=trunc_type)
+xtrainpadded = pad_sequences(xtrainencoded, maxlen=maxInput, padding=padType, truncating=truncType)
 xtrain = np.asarray(xtrainpadded).astype(np.float32)
 ytrain = np.asarray(ytrain).astype(np.float32)
 
 # preparing testing data for neural network
 xtestencoded = tokenizer.texts_to_sequences(xtest)
-xtestpadded = pad_sequences(xtestencoded, maxlen=max_words, padding=padding_type, truncating=trunc_type)
+xtestpadded = pad_sequences(xtestencoded, maxlen=maxInput, padding=padType, truncating=truncType)
 xtest = np.asarray(xtestpadded).astype(np.float32)
 ytest = np.asarray(ytest).astype(np.float32)
 
 # neural network
 model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_words),
+    tf.keras.layers.Embedding(vocabSize, outputDim, input_length=maxInput),
     tf.keras.layers.GlobalAveragePooling1D(),
     tf.keras.layers.Dense(24, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
-num_epochs = 30
-history = model.fit(xtrain, ytrain, epochs=num_epochs, validation_data=(xtest, ytest), verbose=2)
+numEpochs = 30
+history = model.fit(xtrain, ytrain, epochs=numEpochs, validation_data=(xtest, ytest), verbose=2)
+model.summary()
+'''
+# graphs
+import matplotlib.pyplot as plt
 
+def plot_graphs(history, string):
+  plt.plot(history.history[string])
+  plt.plot(history.history['val_'+string])
+  plt.xlabel("Epochs")
+  plt.ylabel(string)
+  plt.legend([string, 'val_'+string])
+  plt.show()
+  
+plot_graphs(history, "accuracy")
+plot_graphs(history, "loss")
+'''
+
+# saving trained model for further use
+sentence = ["granny starting to fear spiders in the garden might be real", "damn pilots use autotune too!!!! This shit has gotten out of hand LOL"]
+sequences = tokenizer.texts_to_sequences(sentence)
+padded = pad_sequences(sequences, maxlen=maxInput, padding=padType, truncating=truncType)
+print(model.predict(padded))

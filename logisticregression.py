a384ee882b4d58a9108ba0ed7f9d926baba9b749
kamperemu <pvedang388@gmail.com>
2020-12-08 10:01:19 +0300

    all the damn changes

--- logisticregression.py
+++ logisticregression.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from nltk.tokenize import word_tokenize
 from nltk import pos_tag
 from nltk.corpus import stopwords
@@ -8,10 +7,9 @@ from sklearn.preprocessing import LabelEncoder
 from collections import defaultdict
 from nltk.corpus import wordnet as wn
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn import model_selection, naive_bayes, svm
+from sklearn import model_selection
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-from sklearn.utils import shuffle
 
 
 
@@ -24,46 +22,38 @@ tweets['content'] = [entry.lower() for entry in tweets['content']]
 
 
 
-# Step - c : Tokenization : In this each entry in the corpus will be broken into set of words
+# tokenization algorithm
 tweets['content']= [word_tokenize(entry) for entry in tweets['content']]
 
-tag_map = defaultdict(lambda : wn.NOUN)
-tag_map['J'] = wn.ADJ
-tag_map['V'] = wn.VERB
-tag_map['R'] = wn.ADV
+tagMap = defaultdict(lambda : wn.NOUN)
+tagMap['J'] = wn.ADJ
+tagMap['V'] = wn.VERB
+tagMap['R'] = wn.ADV
 
-
+# processing the data for a better tokenization
 for index,entry in enumerate(tweets['content']):
-    # Declaring Empty List to store the words that follow the rules for this step
-    Final_words = []
-    # Initializing WordNetLemmatizer()
-    word_Lemmatized = WordNetLemmatizer()
-    # pos_tag function below will provide the 'tag' i.e if the word is Noun(N) or Verb(V) or something else.
+    finalWords = []
+    wordLemmatizer = WordNetLemmatizer()
     for word, tag in pos_tag(entry):
-        # Below condition is to check for Stop words and consider only alphabets    
         if word not in stopwords.words('english') and word.isalpha():
-            word_Final = word_Lemmatized.lemmatize(word,tag_map[tag[0]])
-            Final_words.append(word_Final)
-    # The final processed set of words for each iteration will be stored in 'text_final'
-    tweets.loc[index,'text_final'] = str(Final_words)
+            wordFinal = wordLemmatizer.lemmatize(word,tagMap[tag[0]])
+            finalWords.append(wordFinal)
+    tweets.loc[index,'textFinal'] = str(finalWords)
 
 
-tweets = shuffle(tweets)
-xtrain, xtest, ytrain, ytest = model_selection.train_test_split(tweets['text_final'],tweets['label'],test_size=0.3)
+xtrain, xtest, ytrain, ytest = model_selection.train_test_split(tweets['textFinal'],tweets['label'],test_size=0.3)
 
+# encoding the data
 Encoder = LabelEncoder()
 ytrain = Encoder.fit_transform(ytrain)
 ytest = Encoder.fit_transform(ytest)
 Tfidf_vect = TfidfVectorizer(max_features=5000)
-Tfidf_vect.fit(tweets['text_final'])
-xtrain_Tfidf = Tfidf_vect.transform(xtrain)
-xtest_Tfidf = Tfidf_vect.transform(xtest)
+Tfidf_vect.fit(tweets['textFinal'])
+xtrain = Tfidf_vect.transform(xtrain)
+xtest = Tfidf_vect.transform(xtest)
 
-# Classifier - Algorithm - SVM
-# fit the training dataset on the classifier
+# training the classifier algorithm
 clf = LogisticRegression(max_iter=10000)
-clf.fit(xtrain_Tfidf,ytrain)
-# predict the labels on validation dataset
-pred = clf.predict(xtest_Tfidf)
-# Use accuracy_score function to get the accuracy
-print("SVM Accuracy Score -> ",accuracy_score(pred, ytest)*100)
+clf.fit(xtrain,ytrain)
+pred = clf.predict(xtest)
+print("Logistic Regression Accuracy Score -> ",accuracy_score(pred, ytest)*100)
--- naivebayes.py
+++ naivebayes.py
@@ -12,7 +12,6 @@ from sklearn import model_selection, naive_bayes, svm
 from sklearn.metrics import accuracy_score
 
 
-
 # loading and modifying the tweets for the algorithm
 tweets = pd.read_csv("datasets/test.csv")
 
@@ -22,43 +21,38 @@ tweets['content'] = [entry.lower() for entry in tweets['content']]
 
 
 
-# Step - c : Tokenization : In this each entry in the corpus will be broken into set of words
+# tokenization algorithm
 tweets['content']= [word_tokenize(entry) for entry in tweets['content']]
-# Step - d : Remove Stop words, Non-Numeric and perfom Word Stemming/Lemmenting.
-# WordNetLemmatizer requires Pos tags to understand if the word is noun or verb or adjective etc. By default it is set to Noun
-tag_map = defaultdict(lambda : wn.NOUN)
-tag_map['J'] = wn.ADJ
-tag_map['V'] = wn.VERB
-tag_map['R'] = wn.ADV
+
+tagMap = defaultdict(lambda : wn.NOUN)
+tagMap['J'] = wn.ADJ
+tagMap['V'] = wn.VERB
+tagMap['R'] = wn.ADV
+
+# processing the data for a better tokenization
 for index,entry in enumerate(tweets['content']):
-    # Declaring Empty List to store the words that follow the rules for this step
-    Final_words = []
-    # Initializing WordNetLemmatizer()
-    word_Lemmatized = WordNetLemmatizer()
-    # pos_tag function below will provide the 'tag' i.e if the word is Noun(N) or Verb(V) or something else.
+    finalWords = []
+    wordLemmatizer = WordNetLemmatizer()
     for word, tag in pos_tag(entry):
-        # Below condition is to check for Stop words and consider only alphabets
         if word not in stopwords.words('english') and word.isalpha():
-            word_Final = word_Lemmatized.lemmatize(word,tag_map[tag[0]])
-            Final_words.append(word_Final)
-    # The final processed set of words for each iteration will be stored in 'text_final'
-    tweets.loc[index,'text_final'] = str(Final_words)
+            wordFinal = wordLemmatizer.lemmatize(word,tagMap[tag[0]])
+            finalWords.append(wordFinal)
+    tweets.loc[index,'textFinal'] = str(finalWords)
 
-xtrain, xtest, ytrain, ytest = model_selection.train_test_split(tweets['text_final'],tweets['label'],test_size=0.3)
+xtrain, xtest, ytrain, ytest = model_selection.train_test_split(tweets['textFinal'],tweets['label'],test_size=0.3)
 
+# encoding the data
 Encoder = LabelEncoder()
 ytrain = Encoder.fit_transform(ytrain)
 ytest = Encoder.fit_transform(ytest)
 Tfidf_vect = TfidfVectorizer(max_features=5000)
-Tfidf_vect.fit(tweets['text_final'])
-xtrain_Tfidf = Tfidf_vect.transform(xtrain)
-xtest_Tfidf = Tfidf_vect.transform(xtest)
+Tfidf_vect.fit(tweets['textFinal'])
+xtrain = Tfidf_vect.transform(xtrain)
+xtest = Tfidf_vect.transform(xtest)
 
-# Classifier - Algorithm - SVM
-# fit the training dataset on the classifier
+# training the classifier algorithm
 clf = naive_bayes.MultinomialNB()
-clf.fit(xtrain_Tfidf,ytrain)
-# predict the labels on validation dataset
-pred = clf.predict(xtest_Tfidf)
-# Use accuracy_score function to get the accuracy
-print("SVM Accuracy Score -> ",accuracy_score(pred, ytest)*100)
+clf.fit(xtrain,ytrain)
+pred = clf.predict(xtest)
+print("Naive Bayes Accuracy Score -> ",accuracy_score(pred, ytest)*100)
+

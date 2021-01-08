05998a3ab887d71c5430553d8efb109ae912db75
kamperemu <pvedang388@gmail.com>
2021-01-08 21:18:23 +0300

    BERT baby

--- BERTsvmtrain.py
+++ BERTsvmtrain.py
@@ -9,8 +9,7 @@ from nltk.corpus import wordnet as wn
 from sklearn import model_selection, naive_bayes, svm
 from sklearn.metrics import accuracy_score
 import pickle
-from gensim.models import Word2Vec
-
+from transformers import BertTokenizer
 
 # loading and modifying the tweets for the algorithm
 tweets = pd.read_csv("datasets/test.csv")
@@ -40,13 +39,14 @@ for index,entry in enumerate(tweets['content']):
     tweets.loc[index,'textFinal'] = str(finalWords)
 
 xtrain, xtest, ytrain, ytest = model_selection.train_test_split(tweets['textFinal'],tweets['label'],test_size=0.3)
-
-all_words = [word_tokenize(entry) for entry in tweets['textFinal']]
-
-word2vec = Word2Vec(all_words, min_count=2)
-
+tz = BertTokenizer.from_pretrained("bert-base-cased")
+tz.convert_tokens_to_ids(["characteristically"])
+#xtrain = tz.convert_tokens_to_ids(tz.tokenize(xtrain))
+print(xtrain)
+'''
 # training the classifier algorithm
 clf = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
 clf.fit(xtrain,ytrain)
 pred = clf.predict(xtest)
 print("SVM Accuracy Score -> ",accuracy_score(pred, ytest)*100)
+'''

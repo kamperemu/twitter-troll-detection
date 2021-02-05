a0fb94c641873f2cefefa15f68a316b22907c490
kamperemu <pvedang388@gmail.com>
2021-02-05 18:30:48 +0300

    new

--- datasets/csvtojson.py
+++ datasets/csvtojson.py
@@ -1,10 +1,10 @@
 import csv
 import json
 data = []
-with open("test.csv", encoding='utf-8') as csvf:
+with open("new.csv", encoding='utf-8') as csvf:
 	csvReader = csv.DictReader(csvf)
 	for rows in csvReader:
 		data.append(rows)
 	
-with open("test.json", 'w', encoding='utf-8') as jsonf:
+with open("new.json", 'w', encoding='utf-8') as jsonf:
 	jsonf.write(json.dumps(data,indent=4))
